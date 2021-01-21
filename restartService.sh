8377837cebb7a92b8aa3fbef77773e6e61d1a027
LiZeC <tomgreem123@gmail.com>
2021-01-21 18:36:34 +0800

    添加重启脚本

---
+++ restartService.sh
@@ -0,0 +1,9 @@
+echo "杀死当前的Python进程"
+pid=$(ps x | grep "python3.8 app.py" | grep -v grep | awk '{print $1}')
+kill "$pid"
+
+echo "更新数据结构"
+python3 ./tool4convert.py -c
+
+echo "重启Python进程"
+nohup python3.8 app.py > log.txt 2>&1 &
