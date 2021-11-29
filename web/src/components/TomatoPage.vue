423e7ef6db159201c2168018b8abfd9487993fef
LiZeC <tomgreem123@gmail.com>
2021-11-29 18:52:53 +0800

    添加番茄钟跳转功能

--- web/src/components/TomatoPage.vue
+++ web/src/components/TomatoPage.vue
@@ -3,14 +3,17 @@
     <p>开始时间: {{ startTime.toLocaleTimeString() }} </p>
     <p>任务名称: {{ taskName }}</p>
     <p style="text-align:center;font-size: 45px">{{ timeWithMin }}</p>
-    <button style="display:block;margin:0 auto;font-size: 25px;" @click="reset">重置番茄钟</button>
+    <!--    <button style="display:block;margin:0 auto;font-size: 25px;" @click="reset">重置番茄钟</button>-->
   </div>
 </template>
 
 <script>
 
-const resetTime = 5 * 60;
-const tomatoTime = 25 * 60;
+const resetTime = 5 * 60
+const tomatoTime = 25 * 60
+
+const resetTimeMS = 1000 * resetTime
+const tomatoTomeMS = 1000 * tomatoTime
 
 export default {
   name: "TomatoPage",
@@ -19,14 +22,32 @@ export default {
       startTime: new Date(),
       taskName: "任务名",
       timeSeconds: 0,
-      stage: "FOCUS",
+      stage: "DONE",
     }
   },
   mounted() {
-    this.reset()
     setInterval(this.timeHandler, 1000)
 
     document.title = "番茄钟"
+
+    this.axios.get("/tomato/getTask").then(res => {
+      console.log(res.data.data)
+      let d = res.data.data
+      let tsStart = d.startTime * 1000
+      let tsNow = new Date().getTime()
+
+      let delta = tsNow - tsStart
+      if (delta < tomatoTomeMS) {
+        this.stage = "FOCUS"
+        this.timeSeconds = (tomatoTomeMS - delta) / 1000
+      } else if (delta < tomatoTomeMS + resetTimeMS) {
+        this.stage = "REST"
+        this.timeSeconds = (tomatoTomeMS + resetTimeMS - delta) / 1000
+      }
+
+      this.startTime = new Date(d.startTime)
+      this.taskName = d.name
+    })
   },
   computed: {
     timeWithMin: function () {
@@ -54,7 +75,7 @@ export default {
         } else {
           this.stage = "REST"
           this.timeSeconds = resetTime;
-          new Notification("完成一个番茄钟啦~", {body: "任务: "+ this.taskName +" 完成了, 休息一下吧~"})
+          new Notification("完成一个番茄钟了, 休息一下吧~", {body: "任务: " + this.taskName})
         }
       } else if (this.stage === "REST") {
         if (this.timeSeconds > 0) {
@@ -65,11 +86,6 @@ export default {
         }
       }
     },
-    reset: function () {
-      this.stage = "FOCUS"
-      this.timeSeconds = tomatoTime
-      this.startTime = new Date()
-    }
   }
 }
 
