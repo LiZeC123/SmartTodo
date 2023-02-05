c08d5a5b1d1a277b75ff25c6c249912d9262a9ff
LiZeC <tomgreem123@gmail.com>
2023-02-05 21:57:37 +0800

    feat: 发送消息时对每种方法添加错误处理

--- app/tool4mail.py
+++ app/tool4mail.py
@@ -1,18 +1,32 @@
 import yagmail
 import requests
-import json
 
 from service4config import ConfigManager
+from tool4log import logger
 
 config = ConfigManager()
 
 
 def send_message(title, msg, email_address, qw_hook):
+    try:
+        send_message_by_mail(email_address, msg, title)
+    except Exception as e:
+        logger.exception(e)
+
+    try:
+        send_message_by_qw(msg, qw_hook)
+    except Exception as e:
+        logger.exception(e)
+
+
+def send_message_by_qw(msg, qw_hook):
+    if qw_hook:
+        data = {"msgtype": "markdown", "markdown": {"content": msg}}
+        requests.post(qw_hook, json=data)
+
+
+def send_message_by_mail(email_address, msg, title):
     if email_address:
         sender, password = config.get_mail_info()
         yag = yagmail.SMTP(user=sender, password=password, host='smtp.qq.com', smtp_ssl=True)
         yag.send(to=email_address, subject=title, contents=msg)
-    
-    if qw_hook:
-        data =  {"msgtype" : "markdown", "markdown": {"content": msg}}
-        requests.post(qw_hook, json=data)
