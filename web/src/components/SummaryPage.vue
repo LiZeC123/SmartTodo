aee60d5a31d23977b42b0b53c2b02d1139deec37
LiZeC <tomgreem123@gmail.com>
2024-02-17 14:24:13 +0800

    fix: 修复编译失败问题

--- web/src/components/SummaryPage.vue
+++ web/src/components/SummaryPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>统计信息</h2>
-    <p>累计完成番茄钟数量: {{ stats.total.count }} 累计学习时间: {{ stats.total.hour }}小时 日均学习时间: {{ stats.total.average }}分钟</p>
+    <!-- <p>累计完成番茄钟数量: {{ stats.total.count }} 累计学习时间: {{ stats.total.hour }}小时 日均学习时间: {{ stats.total.average }}分钟</p>
     <p>今日完成番茄钟数量: {{ stats.today.count }} 今日累计学习时间: {{ stats.today.minute }}分钟</p>
     <canvas id="myChart"></canvas>
 
@@ -11,138 +11,138 @@
     <item-list title="打卡统计" :btn-config="[]" :data="habitSummary"></item-list>
 
     <h2>日报框架</h2>
-    <MessageBox :message="dailyReport"> </MessageBox>
+    <MessageBox :message="dailyReport"> </MessageBox> -->
 
   </div>
 </template>
 
-<script>
-import ItemList from "@/components/m/ItemList";
-import MessageBox from "@/components/m/MessageBox";
+<script setup lang="ts">
+// import ItemList from "@/components/m/ItemList";
+// import MessageBox from "@/components/m/MessageBox";
 
-export default {
-  name: "SummaryPage",
-  components: {MessageBox, ItemList},
-  props: {
-    updateTodo: Number,
-  },
-  data: function () {
-    return {
-      todaySummary: [],
-      stats: {
-        total: {
-          count: 0,
-          hour: 0,
-          average: 0
-        },
-        today: {
-          count: 0,
-          minute: 0,
-        },
-        week: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      },
-      habitSummary: [],
-      dailyReport: "",
-    }
-  },
-  mounted() {
-    this.reload()
-  },
-  computed: {},
-  methods: {
-    reload: function () {
-      document.title = "任务统计"
+// export default {
+//   name: "SummaryPage",
+//   components: {MessageBox, ItemList},
+//   props: {
+//     updateTodo: Number,
+//   },
+//   data: function () {
+//     return {
+//       todaySummary: [],
+//       stats: {
+//         total: {
+//           count: 0,
+//           hour: 0,
+//           average: 0
+//         },
+//         today: {
+//           count: 0,
+//           minute: 0,
+//         },
+//         week: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       },
+//       habitSummary: [],
+//       dailyReport: "",
+//     }
+//   },
+//   mounted() {
+//     this.reload()
+//   },
+//   computed: {},
+//   methods: {
+//     reload: function () {
+//       document.title = "任务统计"
 
-      this.axios.post("/item/getSummary", {}).then(res => {
-        let ans = []
-        let data = res.data.items
-        for (let key in data) {
-          let mList = data[key]
-          for (let i = 0; i < mList.length; i++) {
-            mList[i].subTask = (i !== 0)
-          }
-          ans = ans.concat(mList)
-        }
-        this.todaySummary = ans
-        this.stats = res.data.stats
-        this.habitSummary = res.data.habit
+//       this.axios.post("/item/getSummary", {}).then(res => {
+//         let ans = []
+//         let data = res.data.items
+//         for (let key in data) {
+//           let mList = data[key]
+//           for (let i = 0; i < mList.length; i++) {
+//             mList[i].subTask = (i !== 0)
+//           }
+//           ans = ans.concat(mList)
+//         }
+//         this.todaySummary = ans
+//         this.stats = res.data.stats
+//         this.habitSummary = res.data.habit
 
-        this.draw()
-      })
+//         this.draw()
+//       })
 
-      this.axios.get("/item/dailyReport").then(res => {
-        console.log(res)
-        this.dailyReport = res.data
-      })
-    },
-    draw: function () {
-      const labels = getDateArray();
+//       this.axios.get("/item/dailyReport").then(res => {
+//         console.log(res)
+//         this.dailyReport = res.data
+//       })
+//     },
+//     draw: function () {
+//       const labels = getDateArray();
 
-      const data = {
-        labels: labels,
-        datasets: [{
-          label: '近期日专注时长统计',
-          backgroundColor: 'rgb(27,141,227)',
-          borderColor: 'rgb(27,141,227)',
-          data: this.stats.week,
-          tension: 0.3,
-        }]
-      };
+//       const data = {
+//         labels: labels,
+//         datasets: [{
+//           label: '近期日专注时长统计',
+//           backgroundColor: 'rgb(27,141,227)',
+//           borderColor: 'rgb(27,141,227)',
+//           data: this.stats.week,
+//           tension: 0.3,
+//         }]
+//       };
 
-      const config = {
-        type: 'line',
-        data: data,
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          },
-        }
-      };
+//       const config = {
+//         type: 'line',
+//         data: data,
+//         options: {
+//           scales: {
+//             y: {
+//               beginAtZero: true
+//             }
+//           },
+//         }
+//       };
 
-      // eslint-disable-next-line no-undef
-      new Chart(
-          document.getElementById('myChart'),
-          config
-      );
-    },
-    findItem: function (index) {
-      return this.todaySummary[index]
-    },
-    increaseUsedTomatoTime: function (index, id) {
-      this.axios.post("/item/increaseUsedTomatoTime", {"id": id}).then(() => {
-        let item = this.findItem(index)
-        if (item.used_tomato < item.expected_tomato) {
-          item.used_tomato += 1
-        }
-      })
-    },
-  },
-  watch: {
-    "updateTodo": function () {
-      this.reload()
-    },
-  }
-}
+//       // eslint-disable-next-line no-undef
+//       new Chart(
+//           document.getElementById('myChart'),
+//           config
+//       );
+//     },
+//     findItem: function (index) {
+//       return this.todaySummary[index]
+//     },
+//     increaseUsedTomatoTime: function (index, id) {
+//       this.axios.post("/item/increaseUsedTomatoTime", {"id": id}).then(() => {
+//         let item = this.findItem(index)
+//         if (item.used_tomato < item.expected_tomato) {
+//           item.used_tomato += 1
+//         }
+//       })
+//     },
+//   },
+//   watch: {
+//     "updateTodo": function () {
+//       this.reload()
+//     },
+//   }
+// }
 
-function getDateArray() {
-  let array = []
-  for (let i = 0; i < 15; i++) {
-    array.push(getDay(-i));
-  }
+// function getDateArray() {
+//   let array = []
+//   for (let i = 0; i < 15; i++) {
+//     array.push(getDay(-i));
+//   }
 
-  return array
-}
+//   return array
+// }
 
-function getDay(change) {
-  let today = new Date()
-  const target = today.getTime() + 1000 * 60 * 60 * 24 * change;
+// function getDay(change) {
+//   let today = new Date()
+//   const target = today.getTime() + 1000 * 60 * 60 * 24 * change;
 
-  today.setTime(target)
+//   today.setTime(target)
 
-  return (today.getMonth() + 1) + "." + (today.getDate())
-}
+//   return (today.getMonth() + 1) + "." + (today.getDate())
+// }
 
 
 </script>
