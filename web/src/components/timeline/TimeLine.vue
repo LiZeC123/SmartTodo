40bc1404119db89d5370d6200961bd8e51f4ee52
LiZeC <LiZeC123@users.noreply.github.com>
2024-02-28 14:34:31 +0000

    feat: 调整时间线展示的样式

--- web/src/components/timeline/TimeLine.vue
+++ web/src/components/timeline/TimeLine.vue
@@ -1,7 +1,7 @@
 <template>
     <h2>任务时间轴</h2>
     <p>已完成 {{ props.count.tomatoCounts }} 个番茄钟, 累计专注 {{ focusHour }} 小时 {{ focusMinute }} 分钟</p>
-    <li v-for="item in fullItems">{{ item.start }} - {{ item.finish }} ({{ elapsedTime(item) }}分钟) {{ item.title }}</li>
+    <li v-for="item in fullItems" :class="mapClass(item)" >{{ item.start }} - {{ item.finish }} ({{ elapsedTime(item) }}分钟) {{ item.title }}</li>
 </template>
 
 
@@ -19,9 +19,16 @@ const focusHour = computed(() => Math.floor(props.count.totalMinutes / 60))
 const focusMinute = computed(() => Math.floor(props.count.totalMinutes % 60))
 const fullItems = computed(() => fullTimeLine(props.items))
 
+function mapClass(item: TimeLineItem): string {
+    if (item.title.includes("===")) {
+        return "empty"
+    } else {
+        return "tomato"
+    }
+}
+
 function elapsedTime(item: TimeLineItem): string {
     const v = timeStrToMinutes(item.finish) - timeStrToMinutes(item.start)
-
     return v.toString().padStart(2, '0')
 }
 
@@ -30,5 +37,16 @@ function elapsedTime(item: TimeLineItem): string {
 <style scoped>
 li {
     font-family: monospace;
+}
+
+.tomato {
+    margin-top: 4px;
+    margin-bottom: 4px;
+}
+
+.empty {
+    opacity: 0.4;
+    margin-top: 8px;
+    margin-bottom: 8px;
 }
 </style>
