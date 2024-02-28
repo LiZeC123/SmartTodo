36d7240e9b2927b760de69444639d94f02e26500
LiZeC <LiZeC123@users.noreply.github.com>
2024-02-28 05:24:40 +0000

    feat: 时间线自动填充番茄钟之间的空隙

--- web/src/components/timeline/TimeLine.vue
+++ web/src/components/timeline/TimeLine.vue
@@ -1,13 +1,14 @@
 <template>
     <h2>任务时间轴</h2>
     <p>已完成 {{ props.count.tomatoCounts }} 个番茄钟, 累计专注 {{ focusHour }} 小时 {{ focusMinute }} 分钟</p>
-    <li v-for="item in props.items">{{ item.start }} - {{ item.finish }}  {{ item.title }}</li>
+    <li v-for="item in fullItems">{{ item.start }} - {{ item.finish }} ({{ elapsedTime(item) }}分钟) {{ item.title }}</li>
 </template>
 
 
 <script setup lang="ts">
 import { computed } from "vue";
 import type { TimeLineItem, CountInfo } from "./types"
+import { fullTimeLine, timeStrToMinutes } from "./convert";
 
 const props = defineProps<{
     items: TimeLineItem[]
@@ -15,15 +16,19 @@ const props = defineProps<{
 }>()
 
 const focusHour = computed(() => Math.floor(props.count.totalMinutes / 60))
-
 const focusMinute = computed(() => Math.floor(props.count.totalMinutes % 60))
+const fullItems = computed(() => fullTimeLine(props.items))
+
+function elapsedTime(item: TimeLineItem): string {
+    const v = timeStrToMinutes(item.finish) - timeStrToMinutes(item.start)
+
+    return v.toString().padStart(3, '0')
+}
 
 </script>
 
 <style scoped>
-
 li {
-    font-family:monospace;
+    font-family: monospace;
 }
-
 </style>
---
+++ web/src/components/timeline/convert.ts
@@ -0,0 +1,51 @@
+import type { TimeLineItem } from "./types";
+
+// 将小时:分钟格式的字符串转换为当天经过的分钟数
+export function timeStrToMinutes(timeStr: string): number {
+    const [hours, minutes] = timeStr.split(':').map(Number);
+    return hours * 60 + minutes;
+}
+
+// 将当天经过的分钟数转换为小时:分钟格式的字符串
+export function minutesToTimeStr(minutes: number): string {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    return `${hours.toString().padStart(2, '0')}:${remainingMinutes.toString().padStart(2, '0')}`;
+}
+
+
+const title = "===空闲时间==="
+
+export function fullTimeLine(items: TimeLineItem[]): TimeLineItem[] {
+    if (items.length === 0) {
+        return items
+    }
+
+    let ans: TimeLineItem[] = []
+    
+    // 判断并插入首节点
+    const s0 = items[0]
+    if (timeStrToMinutes(s0.start) > timeStrToMinutes("10:00")) {
+        ans.push({ start: "10:00", finish: s0.start, title })
+    }
+    ans.push(s0)
+
+
+    // 插入任务之间的间隙节点
+    let last = s0
+    for (let i = 1; i < items.length; i++) {
+        const item = items[i]
+        if(timeStrToMinutes(last.finish) + 5 < timeStrToMinutes(item.start)) {
+            ans.push({start: last.finish, finish: item.start, title})
+        }
+        ans.push(item)
+        last = item
+    }
+
+    // 判断并插入尾结点
+    if(timeStrToMinutes(last.finish) < timeStrToMinutes("21:00")) {
+        ans.push({start: last.finish, finish: "21:00", title})
+    }
+
+    return ans;
+}
