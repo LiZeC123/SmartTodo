116621448142eb4abde585d6feab7015ec42a483
LiZeC <LiZeC123@users.noreply.github.com>
2024-08-19 11:54:23 +0000

    feat: 调整Item列表排列顺序

--- web/src/components/item/sort.ts
+++ web/src/components/item/sort.ts
@@ -1,10 +1,12 @@
 import type { Item } from "./types";
 
+// 排序比较函数, 此函数期望在返回a<b, 并且按照升序排列数据
+// Item需要按照从大到小的顺序排列, 因此这里对结果取反
 export function compareItem(a: Item, b: Item): number {
     const va = calcValue(a)
     const vb = calcValue(b)
 
-    return va - vb
+    return vb - va
 }
 
 
@@ -20,12 +22,12 @@ function calcValue(item: Item): number {
         // 如果距离DL还有7天, 则不提升, 否则距离越近提升越多
         const plus = 7 * dayMs - (dl - base)
 
-        // 减去一定的分数, 分数越小排序越靠前
-        base -= plus
+        // 加上一定的分数, 分数越大排序越靠前
+        base += plus
     }
 
     if (item.expected_tomato === item.used_tomato) {
-        base += 100 * dayMs
+        base -= 100 * dayMs
     }
 
     return base
