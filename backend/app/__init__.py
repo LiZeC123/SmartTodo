21978b9a76106939b1723ae18cf9ffbc630cf2d2
LiZeC <tomgreem123@gmail.com>
2025-02-11 21:54:00 +0800

    fix: 修复番茄钟历史查询失败的问题

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,7 +7,7 @@ from app.models.base import Base
 from app.services.interpreter import OpInterpreter
 from app.services.item_manager import ItemManager
 from app.services.task_manager import TaskManager
-from app.services.tomato_manager import TomatoManager
+from app.services.tomato_manager import TomatoManager, TomatoRecordManager
 from app.tools.token import generate_token_str
 
 # 初始化数据库对象
@@ -16,7 +16,8 @@ engine = create_engine(url='sqlite:///data/data.db', echo=True, future=True)
 # 定义一个基于线程的Session: https://docs.sqlalchemy.org/en/20/orm/contextual.html
 # scoped_session内部使用线程局部变量对每个线程维护一个独立的Session对象. 通常将scoped_session的返回值视为一个函数, 通过函数调用获得内部维护的属于当前线程的Session对象
 # 但scoped_session的返回值本身也进行了代理操作, 可以直接视为一个Session对象
-# TODO: 分析一下这两个auto功能的效果
+# autocommit: 是否在执行完操作后立即自动提交, 通常由于多个操作需要构成一个事务, 因此关闭自动提交
+# autoflush: 是否在必要的时候执行自动刷新. 从文档看可以开, 不过现在保持关闭也可以
 db = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 # 初始化Server层
@@ -24,6 +25,7 @@ config_manager = ConfigManager()
 item_manager = ItemManager(db)
 op_interpreter = OpInterpreter(item_manager)
 tomato_manager = TomatoManager(db, item_manager)
+tomato_record_manager = TomatoRecordManager(db, item_manager)
 
 # 初始化定时任务
 task_manager = TaskManager()
--- backend/app/views/tomato.py
+++ backend/app/views/tomato.py
@@ -2,7 +2,7 @@ from typing import Dict
 
 from flask import Blueprint, request
 
-from app import tomato_manager
+from app import tomato_manager, tomato_record_manager
 from app.views.authority import authority_check
 from app.views.tool import get_xid_from_request
 
@@ -45,3 +45,39 @@ def add_record(owner: str):
     name = f['name']
     start_time = f['startTime']
     return tomato_manager.add_tomato_record(name, start_time, owner)
+
+
+
+@tomato_bp.post('/api/summary/getReport')
+@authority_check()
+def get_summary_items(owner:str):
+    return tomato_record_manager.get_time_line_summary(owner)
+#
+# @app.post('/api/summary/getEventLine')
+# @authority_check()
+# def get_summary_event_line(owner:str):
+#     rst = event_manager.get_today_event(owner)
+#     return [i.to_dict() for i in rst]
+#
+#
+# @app.post('/api/summary/getNote')
+# @authority_check()
+# def get_summary_note(owner:str):
+#     return report_manager.get_today_summary(owner)
+#
+# @app.post('/api/summary/updateNode')
+# @authority_check()
+# def update_summary_note(owner:str):
+#     content = request.get_json().get("content")
+#     return report_manager.update_summary(content, owner)
+#
+# @app.get("/api/summary/getWeeklySummary")
+# @authority_check()
+# def get_report(owner:str):
+#     summarys = report_manager.get_summary_from(this_week_begin())
+#     return [s.to_dict() for s in summarys]
+
+@tomato_bp.post('/api/summary/getSmartReport')
+@authority_check()
+def get_smart_analysis_report(owner:str):
+    return tomato_record_manager.get_smart_analysis_report(owner)
