42b5d6d46bf18105e546b844121dbdf7296c3431
LiZeC <tomgreem123@gmail.com>
2026-03-12 00:14:00 +0800

    feat: 移除发送邮件功能

--- backend/app/tools/mail.py
+++ backend/app/tools/mail.py
@@ -1,4 +1,3 @@
-import yagmail
 import requests
 
 from app.services.config_manager import ConfigManager
@@ -8,10 +7,6 @@ config = ConfigManager()
 
 
 def send_message(title, msg, email_address, qw_hook):
-    try:
-        send_message_by_mail(email_address, msg, title)
-    except Exception as e:
-        logger.exception(e)
 
     try:
         send_message_by_qw(msg, qw_hook)
@@ -25,8 +20,3 @@ def send_message_by_qw(msg, qw_hook):
         requests.post(qw_hook, json=data)
 
 
-def send_message_by_mail(email_address, msg, title):
-    if email_address:
-        sender, password = config.get_mail_info()
-        yag = yagmail.SMTP(user=sender, password=password, host='smtp.qq.com', smtp_ssl=True)
-        yag.send(to=email_address, subject=title, contents=msg)
