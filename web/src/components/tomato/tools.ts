9f632a04b3bf9b66d3e76c691c156198a78b489f
LiZeC <tomgreem123@gmail.com>
2026-03-14 14:01:02 +0800

    feat: 番茄钟结束音乐添加淡入效果

--- web/src/components/tomato/tools.ts
+++ web/src/components/tomato/tools.ts
@@ -1,25 +1,35 @@
 import { OneMinuteMS } from './types'
 
+let fadeInterval: number
+
 export function playNotifacationAudio() {
   const audio = document.getElementById('notificationAudioBase') as HTMLAudioElement
   if (audio) {
     audio.loop = true
+    doFadeIn(audio)
     audio.play()
-    setTimeout(() => audio.pause(), OneMinuteMS / 60 * 99)
+    setTimeout(() => audio.pause(), (OneMinuteMS / 60) * 99)
   }
 }
 
 export function playNotifacationAudioShort() {
   const audio = document.getElementById('notificationAudioShort') as HTMLAudioElement
   if (audio) {
+    doFadeIn(audio)
     audio.play()
   }
 }
 
-export function sendNotification(title: string, body: string) {
-  new Notification(title, { body })
-}
-
-export function sendTomatoNotification(taskName: string) {
-  sendNotification('完成一个番茄钟了, 休息一下吧~', '任务: ' + taskName)
+function doFadeIn(audio: HTMLAudioElement) {
+  audio.volume = 0
+  // 设置淡入效果
+  if (fadeInterval) clearInterval(fadeInterval)
+  fadeInterval = setInterval(() => {
+    if (audio.volume < 0.95) {
+      audio.volume += 0.05
+    } else {
+      audio.volume = 1
+      clearInterval(fadeInterval)
+    }
+  }, 250)
 }
