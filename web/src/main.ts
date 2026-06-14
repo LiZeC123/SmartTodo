cd047aad82f120f71179630de924f211edbd7a5b
lizec <tomgreem123@gmail.com>
2026-06-14 13:11:08 +0800

    fix: 修复依赖问题

--- web/src/main.ts
+++ web/src/main.ts
@@ -7,25 +7,34 @@ import axios from 'axios'
 
 // fontAwesome相关配置
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTrashAlt, faClock, faCheck, faUndo, faAngleDoubleDown, faAngleDoubleUp,  faListOl, faCalculator } from '@fortawesome/free-solid-svg-icons'
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {
+  faTrashAlt,
+  faClock,
+  faCheck,
+  faUndo,
+  faAngleDoubleDown,
+  faAngleDoubleUp,
+  faCalculator
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faTrashAlt, faClock, faCheck, faUndo, faAngleDoubleDown, faAngleDoubleUp, faCalculator)
-
 
 // axios相关配置
 axios.defaults.baseURL = '/api'
 
-axios.interceptors.response.use(res => {
-    return res;
-}, async error=> {
+axios.interceptors.response.use(
+  (res) => {
+    return res
+  },
+  async (error) => {
     if (error.response.status === 401) {
-        console.log("返回401, 跳转到登录页面")
-        await router.push({path: '/login'})
+      console.log('返回401, 跳转到登录页面')
+      await router.push({ path: '/login' })
     } else {
-        console.log("返回错误代码:", error.response.status)
+      console.log('返回错误代码:', error.response.status)
     }
-});
-
+  }
+)
 
 const app = createApp(App)
 
