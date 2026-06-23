f00f06ee104c42cbb6ea32b369bcb58e180ad183
lizec <tomgreem123@gmail.com>
2026-06-23 18:47:17 +0800

    fix: 修复日志页面路径错误的问题

--- web/src/view/TodoPage.vue
+++ web/src/view/TodoPage.vue
@@ -196,7 +196,7 @@ function saveNote(content: string) {
 let footerConfig: FooterConfig[] = [
   { name: '总结列表', needAdmin: false, f: () => router.push({ path: '/summary' }) },
   { name: '上传文件', needAdmin: false, f: selectFile },
-  { name: '查看日志', needAdmin: true, f: () => router.push({ path: '/log/log' }) },
+  { name: '查看日志', needAdmin: true, f: () => router.push({ path: '/log' }) },
   {
     name: '退出登录',
     needAdmin: false,
