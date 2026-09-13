5a1503332bff5f1e8579f8d23632d0d321d7fe17
lizec <tomgreem123@gmail.com>
2026-09-13 16:53:44 +0800

    feat: 减少记忆文本的容量

--- backend/app/models/role.py
+++ backend/app/models/role.py
@@ -5,28 +5,28 @@ from app.models.memory import KB, MemoryPolicy
 MemoryPolicyDict = {
     # 角色设定优先, 适合人设与故事背景会缓慢变化, 但对于对话连续性要求一般的场景
     "S1": MemoryPolicy(
-        enable_role_setting=True, enable_preference=False, max_topic_num=0, max_diary_num=5, raw_content_size=15 * KB
+        enable_role_setting=True, enable_preference=False, max_topic_num=0, max_diary_num=2, raw_content_size=9 * KB
     ),
     # 角色设定优先, 类似于S1, 但对于对话连续性要求更低
     "S2": MemoryPolicy(
-        enable_role_setting=True, enable_preference=False, max_topic_num=0, max_diary_num=3, raw_content_size=9 * KB
+        enable_role_setting=True, enable_preference=False, max_topic_num=0, max_diary_num=1, raw_content_size=6 * KB
     ),
     # 对话连续性优先, 适合人设几乎不变, 话题快速更换的场景, 但需要对整体一致性要求较高的场景
     "D1": MemoryPolicy(
-        enable_role_setting=False, enable_preference=False, max_topic_num=0, max_diary_num=7, raw_content_size=15 * KB
+        enable_role_setting=False, enable_preference=False, max_topic_num=0, max_diary_num=5, raw_content_size=9 * KB
     ),
     # 对话连续性优先, 类似D1, 但导入记忆更少, 更轻量
     "D2": MemoryPolicy(
-        enable_role_setting=False, enable_preference=False, max_topic_num=0, max_diary_num=5, raw_content_size=9 * KB
+        enable_role_setting=False, enable_preference=False, max_topic_num=0, max_diary_num=3, raw_content_size=6 * KB
     ),
     # 话题优先, 适合人设几乎不会变化, 且历史对话没有太大关联, 经常讨论不同话题的场景
     "T1": MemoryPolicy(
-        enable_role_setting=False, enable_preference=False, max_topic_num=12, max_diary_num=5, raw_content_size=5 * KB
+        enable_role_setting=False, enable_preference=False, max_topic_num=12, max_diary_num=2, raw_content_size=5 * KB
     ),
 }
 
 DefaultMemoryPolicy = MemoryPolicy(
-    enable_role_setting=False, enable_preference=False, max_topic_num=0, max_diary_num=0, raw_content_size=10 * KB
+    enable_role_setting=False, enable_preference=False, max_topic_num=0, max_diary_num=0, raw_content_size=6 * KB
 )
 
 
